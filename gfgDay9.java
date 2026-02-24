42d960eaed5a6fc75c8aa05caf5f1662a2c5bb24
Gayatri Jadhav <jadhavgayatri383@gmail.com>
2026-02-24 08:38:40 +0530

    Create gfgDay9.java
    Given two binary arrays, a1[] and a2[] of equal length. Find the length of longest common span (i, j), where i<= j such that a1[i] + a1[i+1] + .... + a1[j] =  a2[i] + a2[i+1] + ... + a2[j].
    
    Examples:
    
    Input: a1[] = [0, 1, 0, 0, 0, 0], a2[] = [1, 0, 1, 0, 0, 1]
    Output: 4
    Explanation: The longest span with same sum is from index 1 to 4 (0-based indexing).

---
+++ gfgDay9.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+
+    public int equalSumSpan(int[] a1, int[] a2) {
+
+        int n = a1.length;
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        int sum1 = 0, sum2 = 0;
+        int maxLen = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum1 += a1[i];
+            sum2 += a2[i];
+
+            int diff = sum1 - sum2;
+
+            if (diff == 0) {
+                maxLen = i + 1;
+            }
+
+            if (map.containsKey(diff)) {
+                maxLen = Math.max(maxLen, i - map.get(diff));
+            } else {
+                map.put(diff, i);
+            }
+        }
+
+        return maxLen;
+    }
+}
